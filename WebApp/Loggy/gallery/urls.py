8041789e40e949866d03c6dadf27a68a45aaf88d
RRibeiro8 <rfribeiro@ua.pt>
2020-03-06 16:31:10 +0000

    Timeline start

--- WebApp/Loggy/gallery/urls.py
+++ WebApp/Loggy/gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (GalleryView)
+from .views import (GalleryView, DateTimeView)
 
 urlpatterns = [
  path('', GalleryView.as_view(), name='gallery'),
+ path('timeline/', DateTimeView.as_view(), name='datetime'),
 ]
--- WebApp/Loggy/gallery/views.py
+++ WebApp/Loggy/gallery/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from fileupload.models import ImageModel
 
+from collections import Counter
+
 class GalleryView(View):
 
 	template_name = "gallery/gallery.html"
@@ -9,5 +11,27 @@ class GalleryView(View):
 
 	def get(self, request):
 		queryset = self.model.objects.all()
+		context = { 'queryset': queryset }
+		return render(request, self.template_name, context)
+
+class DateTimeView(View):
+
+	template_name = "gallery/datetime.html"
+	model = ImageModel
+
+	def get(self, request):
+
+		obj_list = [obj.date_time.strftime('%Y-%m-%d') for obj in self.model.objects.all()]
+		counter = Counter(obj_list)
+		l = [obj for obj in counter]
+		l.sort()
+
+		for i in l:
+			
+			obj = self.model.objects.filter(date_time=)
+			queryset[i] = obj[0].file.url
+
+		queryset = self.model.objects.all()
+
 		context = { 'queryset': queryset }
 		return render(request, self.template_name, context)
